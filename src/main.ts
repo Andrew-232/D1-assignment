7bc4ed803d7a4b2604dac295f1baa2a70ed66471
Andrew-232 <ahern232@ucsc.edu>
2025-10-08 18:47:05 +0000

    Took my clicker code from the previous assignment and added it just to see what it would look like.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+// Simple counter for demonstration
+let counter: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
 
-console.log("hello");
+document.body.innerHTML = `
+  <h1>CMPM 121 Project</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
+`;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  // This looks like to a good place to add some logic!
+  counter++; // Added counter logic
+  counterElement.textContent = counter.toString(); // Added DOM update logic, this is new
+  console.log("I have these thingies:", button, counterElement, counter);
+});
