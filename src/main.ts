9cd685141ebffff5ece64c22f7df88da3cc97c7b
Andrew-232 <ahern232@ucsc.edu>
2025-10-10 23:20:20 +0000

    If everything works the right way then step 5 will be complete. I added the new code to make the button visiable on screen once the player has enough to buy it for the first time.

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,15 @@ document.body.innerHTML = `
 `;
 */
 let counter: number = 0;
+let growthRate: number = 0;
+const upgradeCost: number = 10;
 
 document.body.innerHTML = `
   <h1>Clicker Game</h1>
   <p>Counter: <span id="counter">0</span></p>
+  <p>Growth Rate: <span id="growthRateDisplay">0</span> units/sec</p>
   <button id="increment">Manuel Clicker Button!</button>
+  <button id="purchaseUpgrade" disabled>Purchase Auto Clicker Upgrade (Cost: ${upgradeCost})</button>
 `;
 
 document.body.style.backgroundColor = "lightgrey";
@@ -23,19 +27,23 @@ document.body.style.backgroundColor = "lightgrey";
 // Add click handler
 const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
+const purchaseUpgradeButton = document.getElementById("purchaseUpgrade")!;
+const growthRateDisplay = document.getElementById("growthRateDisplay")!;
 
 button.addEventListener("click", () => {
   counter++;
   counterElement.textContent = counter.toString();
 });
 
-// Added set interval where the counter hould increase by 1 every second.
-//Will comment out to complete step 4
-// setInterval(() => {
-//   counter++;
-//   console.log("setInterval code is working");
-//   counterElement.textContent = counter.toString();
-// }, 1000);
+//Logic for the upgrade
+purchaseUpgradeButton.addEventListener("click", () => {
+  if (counter >= upgradeCost) {
+    counter -= upgradeCost;
+    growthRate++;
+    growthRateDisplay.textContent = growthRate.toString();
+    console.log("Upgrade bought!");
+  }
+});
 
 let currentFrame: number = 0;
 
@@ -49,14 +57,12 @@ function gameLoop(timestamp: number) {
 
   currentFrame = timestamp;
 
-  const growthThisFrame = 1 * deltaTime;
-
-  counter += growthThisFrame;
+  counter += growthRate * deltaTime;
 
   counterElement.textContent = Math.floor(counter).toString();
 
   requestAnimationFrame(gameLoop);
-  console.log("requestion animation frame function is hopefully working!");
+  console.log("Upgrade Logic is working?");
 }
 
 requestAnimationFrame(gameLoop);
